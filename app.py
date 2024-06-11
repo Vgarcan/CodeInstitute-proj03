8363f968b1c41ab73262db85477d90a87c2aa955
Victor GC <vgarcanuk@gmail.com>
2024-06-11 15:18:07 +0100

    FIX: Heroku port and other keys adjusted

--- app.py
+++ app.py
@@ -151,4 +151,7 @@ def appl_desc(appl_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
