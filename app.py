da506b6af12e1637caa10df468151802d3fd4202
Victor GC <vgarcanuk@gmail.com>
2024-06-11 15:43:08 +0100

    UPDATE: Added SecretKey to the project's env

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 ### Extra Functionalities ###
 def obtainCollection(selected_collection, comm_query, id=None):
